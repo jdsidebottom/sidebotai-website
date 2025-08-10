27d4fee9fdd9891bbc3d625cbdb9c487fb7b44bd
jdsidebottom <jdsidebottom@gmail.com>
2025-08-10 16:05:22 -0700

    header logo change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
             <img 
               src="https://rbhvcwzjvgatesivsxbb.supabase.co/storage/v1/object/public/assets/SidebotaiLogoIcon.png" 
               alt="Sidebot AI Logo" 
-              className="w-[100px] h-[100px] md:w-[50px] md:h-[50px]"
+              className="w-[150px] h-[150px] md:w-[50px] md:h-[50px]"
             />
             <div className="text-2xl font-bold">
               <span className="text-secondary">Side</span><span className="text-white">bot</span> <span className="text-secondary">AI</span> 
